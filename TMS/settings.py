753e4db26174306f29569079d6741584b0f43a89
Luther Isaboke <lusaboke@gmail.com>
2024-12-05 16:36:15 +0300

    renamed app to tms

--- TMS/settings.py
+++ TMS/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'Tms',  # Ensure this app is correctly named in your project
+    'tms',  # Ensure this app is correctly named in your project
 ]
 
 MIDDLEWARE = [
