3cbf355c5a3118db4f515952a80bf2496715794c
Luther Isaboke <lusaboke@gmail.com>
2024-12-09 00:30:03 +0300

    updated navigation

--- taskms/views.py
+++ taskms/views.py
@@ -1,24 +1,22 @@
-from django.shortcuts import render,redirect, get_object_or_404
-from django.contrib.auth import login, authenticate,logout
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignupForm, LoginForm
 from django.http import HttpResponse
-from .models import *
+from .models import Project, Task
 import logging
+from django.utils import timezone
 
-#logger setup
-logger= logging.getLogger(__name__)
+# Logger setup
+logger = logging.getLogger(__name__)
 
-
-# Create your views here.
-#homeview
+# Home view
 def home(request):
-    return render(request, 'home.html')
+    current_date = timezone.now().strftime("%B %d, %Y")  # Format: "April 15, 2022"
+    return render(request, 'home.html', {'current_date': current_date})
 
-
-#create signup view
-
+# Signup view
 def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -30,26 +28,25 @@ def signup_view(request):
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
 
-
-#create login view
+# Login view
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home') 
+            return redirect('home')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
-# create the logout view
+# Logout view
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('home')
 
-#create a new project and log
+# Create a new project
 @login_required
 def create_project(request):
     if request.method == 'POST':
@@ -60,16 +57,15 @@ def create_project(request):
         logger.info(f'Project {name} created by {request.user}')
         return redirect('project_list')
     return render(request, 'create_project.html')
-    
 
-#List of projects and log
+# List of projects
 @login_required
 def project_list(request):
     projects = Project.objects.filter(owner=request.user)
     logger.info(f'Projects listed by {request.user}')
     return render(request, 'project_list.html', {'projects': projects})
 
-#Edit projects and log
+# Edit a project
 @login_required
 def edit_project(request, project_id):
     project = get_object_or_404(Project, id=project_id, owner=request.user)
@@ -83,17 +79,15 @@ def edit_project(request, project_id):
         return redirect('project_list')
     return render(request, 'edit_project.html', {'project': project})
 
-
-#Delete projects and log
+# Delete a project
 @login_required
 def delete_project(request, project_id):
     project = get_object_or_404(Project, id=project_id, owner=request.user)
-    project.delete()
     logger.info(f'Project {project.name} deleted by {request.user}')
+    project.delete()
     return redirect('project_list')
 
-
-#create a task for project
+# Create a task for a project
 @login_required
 def create_task(request, project_id):
     project = get_object_or_404(Project, id=project_id, owner=request.user)
@@ -105,37 +99,34 @@ def create_task(request, project_id):
         return redirect('task_list', project_id=project.id)
     return render(request, 'create_task.html', {'project': project})
 
-#Read/list tasks and log
+# List tasks for a project
 @login_required
-def task_list(request,project_id):
+def task_list(request, project_id):
     project = get_object_or_404(Project, id=project_id, owner=request.user)
     tasks = project.tasks.all()
     logger.info(f'Tasks listed for project {project.name} by {request.user.username}')
     return render(request, 'task_list.html', {'project': project, 'tasks': tasks})
 
-#Edit tasks and log
+# Edit a task
 @login_required
 def edit_task(request, task_id, project_id):
     task = get_object_or_404(Task, id=task_id, project__owner=request.user)
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
-        task.status= request.POST.get('status')
+        status = request.POST.get('status')
         task.title = title
         task.description = description
+        task.status = status
         task.save()
         logger.info(f'Task {task.title} updated by {request.user.username}')
         return redirect('task_list', project_id=project_id)
     return render(request, 'edit_task.html', {'task': task})
 
-
-#delete tasks and log
+# Delete a task
 @login_required
-def delete_task(request, task_id,project_id):
+def delete_task(request, task_id, project_id):
     task = get_object_or_404(Task, id=task_id, project__owner=request.user)
-    project_id = task.project.id
-    task.delete()
     logger.info(f'Task {task.title} deleted by {request.user.username}')
+    task.delete()
     return redirect('task_list', project_id=project_id)
-
-
